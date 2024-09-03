97324df63c0bb3d873c31d31eea36b1682ed8108
Gall Steinitz <gall.steinitz@datagrail.io>
2024-09-02 21:55:01 -0700

    added link to pasts page

--- app/javascript/PostRow.tsx
+++ app/javascript/PostRow.tsx
@@ -4,7 +4,7 @@ import { Post } from './types';
 const PostRow: React.FC<{ post: Post }> = ({ post }) => {
   return (
     <li role="listitem">
-      {post.title}
+      <a href={`/posts/${post.id}`}>{post.title}</a>
     </li>
   );
 }
