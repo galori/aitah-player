efe297c79ea9b27b64923d816849356ad7f9a8f5
Gall Steinitz <gall.steinitz@datagrail.io>
2024-09-07 17:32:23 -0700

    posts page back with roboto this time

--- app/javascript/index.tsx
+++ app/javascript/index.tsx
@@ -6,18 +6,29 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import TestPage from './components/TestPage';
+import PostsPage from "./PostsPage";
+import PostPage from "./PostPage";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const theme = createTheme();
+
+const root = document.getElementById('root');
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
     <React.StrictMode>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <TestPage />
+        <Router>
+          <div>
+            <Routes>
+              <Route path="/" element={< PostsPage />} />
+              <Route path="/post/:id" element={< PostPage />} />
+            </Routes>
+          </div>
+        </Router>
       </ThemeProvider>
     </React.StrictMode>,
-    document.body.appendChild(document.createElement('div')),
+    root
   );
 });
