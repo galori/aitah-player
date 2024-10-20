c1238b491c3281df1ecdc4c50302a4db900d5dd7
Gall Steinitz <gall.steinitz@datagrail.io>
2024-10-19 23:23:51 -0700

    WIP

--- app/lib/text_util.rb
+++ app/lib/text_util.rb
@@ -1,5 +1,9 @@
 class TextUtil
-  def self.find_sentences(text)
-    text.gsub(/(?<!\b\w)\.(?!\w\b|$)/, '[SPLIT]').gsub(/\.$/, '')
+  def self.find_sentences(_text)
+    text = _text.dup
+    text.gsub!(/\.{2,}/, '.') # change eclipses to one period
+    text.gsub(/(?<!\b\w)\.(?!\w\b|$)/, '[SPLIT]') # change dots in sentences to [SPLIT] but not in acronyms
+    text.gsub!(/\.$/, '') # remove trailing dot
+    text
   end
 end
--- spec/lib/text_util_spec.rb
+++ spec/lib/text_util_spec.rb
@@ -8,5 +8,10 @@ RSpec.describe TextUtil do
       text = "I love this site a.m. and F.B.I for sure. But I don't like this site. Hi dude."
       expect(TextUtil.find_sentences(text)).to eq("I love this site a.m. and F.B.I for sure[SPLIT] But I don't like this site[SPLIT] Hi dude")
     end
+
+    it 'changes eclipses to one period' do
+      text = "I like ice cream.... I like cake."
+      expect(TextUtil.find_sentences(text)).to eq("I like ice cream. I like cake.")
+    end
   end
 end
